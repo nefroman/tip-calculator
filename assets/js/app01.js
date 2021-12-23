028102ed0f81e736979ef7847954979cf6addf7f
juan bermudez <juanmbescobar@hotmail.com>
2021-12-22 19:41:24 -0500

    variables

--- assets/js/app01.js
+++ assets/js/app01.js
@@ -1,2 +1,17 @@
-console.log("No la cagué");
-console.log("joda");
+//Variables 
+
+const d = document;
+
+const bill = d.getElementsByClassName('input-bill');
+
+const numb_people = d.getElementsByClassName("input-control");
+
+const buttons_percent = d.querySelectorAll(".caja");
+
+const custom_button = d.getElementsByClassName("custom");
+
+const reset_button = d.getElementsByClassName("reset")
+
+const tip = d.getElementsByClassName("propina");
+
+const total = d.getElementsByClassName("personas");
