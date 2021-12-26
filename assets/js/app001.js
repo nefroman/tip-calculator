5bc779c08fa0b24e72f41107a36f5c35331b0311
juan bermudez <juanmbescobar@hotmail.com>
2021-12-26 11:35:35 -0500

    adding functions onclick

---
+++ assets/js/app001.js
@@ -0,0 +1,129 @@
+//variables
+
+const d = document;
+
+
+const bill = d.querySelector('.input-bill')
+const boton_custom = d.querySelector('.custom');
+const botones = d.querySelectorAll('.caja');
+const numb_people = d.querySelector('.input-control');
+const tip_amount = d.querySelector('.propina .dollar');
+const total = d.querySelector('.personas .dollar')
+const boton_reset = d.querySelector('.reset ');
+
+
+//funciones
+function five_percent () {
+    const bill = d.querySelector('.input-bill').value;
+    bill_num = Number.parseFloat(bill);
+    const numb_people  = d.querySelector('.input-control').value;
+    numb_people_num = Number.parseFloat(numb_people);
+    calcularTipAmount(bill_num, numb_people_num);
+    resetAll();
+}
+
+function calcularTipAmount (num, personas) { 
+        let resultado = (num * 0.05)/personas;
+        let resultadoTotal = (num * 1.05)/personas;
+        if (isNaN(resultado)) return
+        else  {
+            tip_amount.innerHTML = resultado;
+            total.innerHTML = resultadoTotal;
+        }
+}
+
+function ten_percent () {
+    const bill = d.querySelector('.input-bill').value;
+    bill_num = Number.parseFloat(bill);
+    const numb_people  = d.querySelector('.input-control').value;
+    numb_people_num = Number.parseFloat(numb_people);
+    
+    
+    calcularTipAmountTen(bill_num, numb_people_num);
+    resetAll();
+}
+
+function calcularTipAmountTen (num, personas) { 
+        let resultado = (num * 0.1)/personas;
+        let resultadoTotal = (num * 1.10)/personas;
+        if (isNaN(resultado)) return
+        else  {
+            tip_amount.innerHTML = resultado;
+            total.innerHTML = resultadoTotal;
+        }
+}
+
+function fifteen_percent () {
+    const bill = d.querySelector('.input-bill').value;
+    bill_num = Number.parseFloat(bill);
+    const numb_people  = d.querySelector('.input-control').value;
+    numb_people_num = Number.parseFloat(numb_people);
+    
+    
+    calcularTipAmountFifteen(bill_num, numb_people_num);
+    resetAll();
+}
+
+function calcularTipAmountFifteen (num, personas) { 
+        let resultado = (num * 0.15)/personas;
+        resultadoTotal = (num * 1.15)/personas;
+        if (isNaN(resultado)) return
+        else  {
+            tip_amount.innerHTML = resultado;
+            total.innerHTML = resultadoTotal;
+        }
+}
+
+function Twentyfive_percent () {
+    const bill = d.querySelector('.input-bill').value;
+    bill_num = Number.parseFloat(bill);
+    const numb_people  = d.querySelector('.input-control').value;
+    numb_people_num = Number.parseFloat(numb_people);
+    
+    
+    calcularTipAmountTwentyFive(bill_num, numb_people_num);
+    resetAll();
+}
+
+function calcularTipAmountTwentyFive (num, personas) { 
+        let resultado = (num * 0.25)/personas;
+        resultadoTotal = (num * 1.25)/personas;
+        if (isNaN(resultado)) return
+        else  {
+            tip_amount.innerHTML = resultado;
+            total.innerHTML = resultadoTotal;
+        }
+}
+
+function fifty_percent () {
+    const bill = d.querySelector('.input-bill').value;
+    bill_num = Number.parseFloat(bill);
+    const numb_people  = d.querySelector('.input-control').value;
+    numb_people_num = Number.parseFloat(numb_people);
+    
+    
+    calcularTipAmountFifty(bill_num, numb_people_num);
+    resetAll();
+}
+
+function calcularTipAmountFifty (num, personas) { 
+        let resultado = (num * 0.50)/personas;
+        resultadoTotal = (num * 1.50)/personas;
+        if (isNaN(resultado)) return
+        else  {
+            tip_amount.innerHTML = resultado;
+            total.innerHTML = resultadoTotal;
+        }
+}
+
+function resetAll(){
+    boton_reset.addEventListener('click', () => {
+        tip_amount.innerHTML = 0;
+        total.innerHTML = 0;
+        bill.value = '';
+        numb_people.value = '';
+    })
+}
+
+
+
